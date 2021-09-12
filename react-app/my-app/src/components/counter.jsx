1f64a3bb7943249460f1ab48a9439e7d3c079983
himanchalsingh93@gmail.com <himanchalsingh93@gmail.com>
2021-09-12 15:47:11 +0530

    removed local scope

--- react-app/my-app/src/components/counter.jsx
+++ react-app/my-app/src/components/counter.jsx
@@ -1,44 +1,48 @@
 import React, { Component } from 'react';
 
-class Counter extends React.Component {
-    state = {
-        value: this.props.value,
-        imageUrl: "https://picsum.photos/300",
-        tags: ['tag1', 'tag2', 'tag3']
-    }; // it's a special property that includes any data that this component needs.
+class Counter extends Component {
+    // state = {
+    //     value: this.props.counter.value,
+    //     //id: this.props.id,
+    //     imageUrl: "https://picsum.photos/300",
+    //     tags: ['tag1', 'tag2', 'tag3']
+    // }; // it's a special property that includes any data that this component needs.
 
     styles = {
         fontSize: 12,
         fontWeight: 'bold'
     }
-    handleIncrement = () => { //arrow function to bind event handlers
-        console.log('Increment clicked', this);
-        this.setState({ value: this.state.value + 1 });
-    }
+    // handleIncrement = () => { //arrow function to bind event handlers
+    //     console.log('Increment clicked', this);
+    //     this.setState({ value: this.state.value + 1 });
+    // }
     render() { 
         return (
-            <React.Fragment>
+            <div>
+                {this.props.children}
                 {/* <img src={this.state.imageUrl} alt="" /> */}
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span> 
-                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
-                <div>{this.renderTagsConditionally()}</div>
-            </React.Fragment>
+                <button onClick={ () => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={ () => this.props.onDelete(this.props.counter.id)} 
+                className="btn btn-danger btn-sm m-2">Delete</button>
+                {/* <div>{this.renderTagsConditionally()}</div> */}
+            </div>
         );
          //jsx expression, it's not a string 
         //which gets compiled into React.createElement()
     }
-    renderTagsConditionally(){
-        if (this.state.tags.length === 0) return <p>there are not tags!</p>
-        return <ul> {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
-    }
+    // renderTagsConditionally(){
+    //     if (this.state.tags.length === 0) return <p>there are not tags!</p>
+    //     return <ul> {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    // }
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.value === 0 ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount(){
-        const {value: count} = this.state;
+        const {value: count} = this.props.counter;
         return count === 0 ? 'Zero' : count; //we can also return a jsx expression like <h1>Zero</h1>
     }
 }
--- react-app/my-app/src/components/counters.jsx
+++ react-app/my-app/src/components/counters.jsx
@@ -1,20 +1,52 @@
 import React, { Component } from 'react';
 import Counter from './counter';
 
-class Counters extends React.Component {
+class Counters extends Component {
     state = {
         counters : [
             {id: 1, value: 4},
             {id: 2, value: 0},
-            {id: 3, value: 120},
+            {id: 3, value: 0},
             {id: 4, value: 0}
         ]
     }
+
+    handleDelete = counterId => { //handling the event
+        console.log('event handler called', counterId);
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({counters}); //overwriting the state
+    }
+
+    handleIncrement = counter => {
+        console.log(counter);
+        const counters = [...this.state.counters]; //cloning the array with spread operator
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value++;
+        this.setState({counters});
+    }
+
+    handleReset = () => {
+        const counters = [...this.state.counters]; //cloning the array with spread operator
+        counters.map(c => c.value = 0);
+        this.setState({counters});
+        console.log(counters);
+    }
+
     render() { 
-        return (<div>
-            {this.state.counters.map(counter => 
-            <Counter key ={counter.id} value={counter.value} selected={true}/>)} 
-        </div>);
+        return (
+            <div>
+                <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
+                {this.state.counters.map(counter => (
+                <Counter key ={counter.id} 
+                onDelete={this.handleDelete} 
+                onIncrement={this.handleIncrement}
+                counter = {counter}>
+                <h4>Counter # {counter.id}</h4>
+                </Counter>)
+                )} 
+            </div>
+        );
     }
 }
  
